efa629b752ed73ed04559d1093fe3b6485bd039c
lautaro rodriguez <lautarorodriguezovalle@gmail.com>
2023-01-03 18:01:55 -0300

    probando fetch2

--- trabajo1/entrega/src/components/Detail/index.jsx
+++ trabajo1/entrega/src/components/Detail/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import DetailMap from '../DetailMap'
+
 
 const Detail = ({productos}) => {
     return (
         <div>
-            {productos.map(producto=>{
-                return <DetailMap map={producto} ></DetailMap>
-            })}
+            <h1>{productos.title}</h1>
         </div>
     )
 }
